7d3b802bdac4ba6a1080efdc21c8f327f60a950d
Enrique <arq.megreyes@gmail.com>
2026-02-24 17:27:42 -0600

    loops new

--- loops.js
+++ loops.js
@@ -13,14 +13,20 @@ array.forEach(array => {
 })
 
 */
-
+/*
 let button1 = document.getElementById('button1')
 let button2 = document.getElementById('button2')
 let button3 = document.getElementById('button3')
 let input = document.getElementById('input')
 let p = document.getElementById('p')
 let p2 = document.getElementById('p2')
-let array = []
+let array = [
+
+{nombre:"spiderman2", year:2008, gender:action}, {nombre:"spiderman2", year:2008, gender:action}, 
+{nombre:"spiderman2", year:2008, gender:action}, {nombre:"spiderman2", year:2008, gender:action}, 
+
+
+]
 
 
 
@@ -64,3 +70,55 @@ button3.addEventListener('click',displaycart)
 
 
 
+console.log(array)   */
+
+
+let button1 = document.getElementById('button1')
+
+let p = document.getElementById('p')
+
+
+
+
+let señorAnillos = {
+    nombre: "the lord of the rings 1",
+    gender:"action",
+    year:2002
+
+}
+
+let matilda = {
+    nombre: "matilda",
+    gender:"drama",
+    year: 1999
+
+}
+
+let ninjaT = {
+    nombre: "ninjaT",
+    gender:"action",
+    year: 1997
+
+}
+
+
+
+
+
+let array = [señorAnillos, matilda, ninjaT
+]
+
+
+
+
+
+function randomMovie () {
+    
+        let randomIndex = Math.floor(Math.random()* array.length)
+        let randomValue = array[randomIndex]
+        let allValues = Object.values(randomValue) 
+        p.innerText = "THIS IS THE MOVIE     " + allValues ;
+}
+
+
+button1.addEventListener("click",randomMovie)
