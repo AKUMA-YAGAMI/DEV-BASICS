99a5c3ed77573e788ee77a2042e2609f778720ae
Enrique <arq.megreyes@gmail.com>
2026-03-09 20:40:58 -0600

    async/await

---
+++ async-practice.js
@@ -0,0 +1,112 @@
+/*
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/8')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+
+
+  fetch('https://jsonplaceholder.typicode.com/todos/8')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json()
+  })
+  .then(data => console.log(data))
+  .catch(error => console.log('Fetch error:', error))
+
+
+
+
+  async function getTodo() {
+  try {
+    let response1 = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    
+    if (!response1.ok) {
+      throw new Error('Network response was not ok')
+    }
+    
+    let response2 = await response1.json()
+    
+    console.log(response2)
+    
+  } catch (error) {
+    console.log('Fetch error:', error)
+  }
+}
+
+getTodo() */
+
+/*
+
+let button = document.getElementById('button')
+let p = document.getElementById('p')
+
+
+  async function getTodo() {
+  try {
+    let response1 = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+    
+    if (!response1.ok) {
+      throw new Error('Network response was not ok')
+    }
+    
+    let response2 = await response1.json()
+    
+   p.innerHTML = `${response2.name} | Type: ${response2.types[0].type.name}`
+    
+  } catch (error) {
+    console.log('Fetch error:', error)
+  }
+
+
+
+}
+
+
+button.addEventListener('click', getTodo)
+console.log("Button found:", button)
+
+*/
+
+let button = document.getElementById('button')
+let p = document.getElementById('p')
+
+async function dogsBreed(){
+try{
+let response1 = await fetch('https://openlibrary.org/search.json?q=the+hunger+games')
+
+if(!response1.ok){
+    throw new Error('FATAL ERROR')
+}
+let response2 = await response1.json()
+
+console.log(response2)
+p.innerHTML = `Just look at this master no hands hahahaha  ${response2.docs[0].title} what else ${response2.docs[0].author_name[0]}`
+
+
+}
+
+catch(error){
+    console.log('error master', error)
+}
+
+}
+
+button.addEventListener('click', dogsBreed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
